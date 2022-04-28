9a93e4dc5219689c7ab9ea162d2ca5b88036c829
ElSicarius <amurat654@gmail.com>
2022-04-28 11:11:22 +0200

    Updating base tampers to include the unprocess function

--- tampers/jwtEncode.py
+++ tampers/jwtEncode.py
@@ -1,6 +1,9 @@
 
 import jwt
 
+KEY = "example_key"
+ALGO = "HS256"
+
 def process(payload):
 	if isinstance(payload, bytes):
 		try:
@@ -11,7 +14,20 @@ def process(payload):
 						# change the json according to your jwt setup
 						{"id": "1", "name":"test","description":payload,"email":"thisisatest@test.test"},\
 						# Enter your key below
-						'example_key',\
+						KEY,\
 						# change the algorithm if needed
-						algorithm='HS256')
+						algorithm=ALGO)
+	return payload
+
+def unprocess(payload):
+	if isinstance(payload, bytes):
+		try:
+			payload = payload.decode("utf8")
+		except UnicodeDecodeError:
+			print("Invalid payload {}, skipping".format(payload))
+	payload = jwt.decode(payload,
+						# Enter your key below
+						KEY,\
+						# change the algorithm if needed
+						algorithms=[ALGO])
 	return payload
