1161ed2d24e9ead96ba21277e9855fefb93ab631
ElSicarius <amurat654@gmail.com>
2022-04-28 10:45:38 +0200

    adding unprocess tamper

--- tampers/base64.py
+++ tampers/base64.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 
-from base64 import b64encode
+from base64 import b64encode, b64decode
 
 def process(payload):
     if not isinstance(payload, bytes):
         return b64encode(payload.encode("utf-8")).decode("utf8")
     else:
          return b64encode(payload).decode("utf8")
+
+def unprocess(payload):
+    if not isinstance(payload, bytes):
+        return b64decode(payload.encode("utf-8")).decode("utf8")
+    else:
+         return b64decode(payload).decode("utf8")
